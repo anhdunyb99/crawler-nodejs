fc78ec9db868220058326b2d045d62518556d048
anhdunyb99 <anhdunyb99@gmail.com>
2023-09-25 19:48:33 +0700

    update-craw-91md

--- crawler-function/crawler_function_91md.ts
+++ crawler-function/crawler_function_91md.ts
@@ -1,7 +1,7 @@
 import { JSDOM } from "jsdom";
 import puppeteer from "puppeteer";
 import { createObjectCsvWriter } from "csv-writer";
-let dom: JSDOM; // Đặt biến dom ở ngoài hàm crawlWebsite
+let dom: JSDOM;
 let browser: any;
 let page: any;
 
@@ -39,7 +39,7 @@ export async function crawlDataFromSinglePage(url: string) {
   console.log(url);
 
   await initializeBrowserAndPage();
-  await page.goto(url, { timeout: 60000 });
+  await page.goto(url, { timeout: 90000 });
   const pageContent = await page.content();
   dom = new JSDOM(pageContent);
 
@@ -111,11 +111,72 @@ export async function crawlAllFrom91Md() {
   /* data = await crawlDataFromSingleStudio(
     "https://91md.me/index.php/vod/type/id/26.html"
   ); */
-  const csvWriter = createObjectCsvWriter({
-    path: "csv/91md_code_film.csv",
-    header: [{ id: "data", title: "Data" }],
+  const actorMap: any = {};
+
+  data.forEach((item) => {
+    const [codeName, actorName] = item.split(".");
+    if (!actorMap[actorName]) {
+      actorMap[actorName] = [];
+    }
+    actorMap[actorName].push(codeName);
   });
 
-  const records = data.map((data) => ({ data }));
+  const records: any = [];
+
+  for (const actorName in actorMap) {
+    records.push({
+      actorName,
+      codeName: actorMap[actorName].join(", "),
+    });
+  }
+
+  const csvWriter = createObjectCsvWriter({
+    path: "csv/91md_code_film.csv",
+    header: [
+      { id: "actorName", title: "Actor Name" },
+      { id: "codeName", title: "Code Name" },
+    ],
+  });
+  await csvWriter.writeRecords(records);
+}
+
+export async function test() {
+  const data = [
+    "MKY-SV-007.白靖寒",
+    "MM-051.台湾第一女优吴梦梦",
+    "MKY-SL-007.白靖寒",
+    "MD-0225.玥可岚",
+    "MDX-0241-04.玥可岚",
+    "MCY-0045.白靖寒",
+    "MM-050.台湾第一女优吴梦梦",
+  ];
+
+  const actorMap: any = {};
+
+  data.forEach((item) => {
+    const [codeName, actorName] = item.split(".");
+    if (!actorMap[actorName]) {
+      actorMap[actorName] = [];
+    }
+    actorMap[actorName].push(codeName);
+  });
+
+  const records: any = [];
+
+  for (const actorName in actorMap) {
+    records.push({
+      actorName,
+      codeName: actorMap[actorName].join(", "),
+    });
+  }
+  console.log(records);
+  const csvWriter = createObjectCsvWriter({
+    path: "csv/91code.csv",
+    header: [
+      { id: "actorName", title: "Actor Name" },
+      { id: "codeName", title: "Code Name" },
+    ],
+  });
+
   await csvWriter.writeRecords(records);
 }
--- index.ts
+++ index.ts
@@ -4,12 +4,14 @@ import {
   crawlAllFrom91Md,
   crawlDataFromSinglePage,
   crawlDataFromSingleStudio,
+  test,
 } from "./crawler-function/crawler_function_91md";
 async function main() {
   /* await crawlAll(); */
   /* await crawlActorName(); */
   /* await collectLinkFromNavBar(); */
   await crawlAllFrom91Md();
+  /* await test(); */
 }
 
 main();
